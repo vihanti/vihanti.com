c729c8ff5712a847ee2c7a49c88a9698bf61821d
Jonathan Van Eenwyk <jonathan@vihanti.com>
2020-05-02 16:39:40 -0700

    Add sitemap

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const SitemapPlugin = require('sitemap-webpack-plugin').default
 
 module.exports = {
   configureWebpack: {
@@ -12,7 +13,23 @@ module.exports = {
       }),
       new FaviconsWebpackPlugin({
         logo: './src/assets/logo2.png'
-      })
+      }),
+      new SitemapPlugin('https://www.vihanti.com',
+        [
+          '/',
+          '/about',
+          '/careers',
+          '/contact',
+          '/privacy',
+          '/services',
+          '/terms'
+        ],
+        {
+          fileName: 'sitemap.xml',
+          lastMod: true,
+          changeFreq: 'monthly'
+        }
+      )
     ]
   },
   css: {
